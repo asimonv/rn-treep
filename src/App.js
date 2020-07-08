9ba032ac6d259d57f40f61c5a111f4e40eedadea
asimonv <aasimon@uc.cl>
2020-07-08 15:22:27 -0400

    fixes vibration bug in android

--- src/App.js
+++ src/App.js
@@ -6,23 +6,32 @@
  * @flow
  */
 
-import React from 'react';
-import { ActivityIndicator, Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { ActionSheetProvider } from '@expo/react-native-action-sheet';
+import React from "react";
+import {
+  ActivityIndicator,
+  Platform,
+  StatusBar,
+  StyleSheet,
+  View,
+} from "react-native";
+import { ActionSheetProvider } from "@expo/react-native-action-sheet";
 
-import { InAppNotificationProvider } from 'react-native-in-app-notification';
-import { PersistGate } from 'redux-persist/integration/react';
-import { Provider } from 'react-redux';
-import Store from './store';
+import { InAppNotificationProvider } from "react-native-in-app-notification";
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
+import Store from "./store";
 
-import AppNavigator from './navigation/AppNavigator';
+import AppNavigator from "./navigation/AppNavigator";
 
 const App = () => (
   <ActionSheetProvider>
     <View style={styles.container}>
-      {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+      {Platform.OS === "ios" && <StatusBar barStyle="dark-content" />}
       <Provider store={Store.store}>
-        <PersistGate loading={<ActivityIndicator />} persistor={Store.persistor}>
+        <PersistGate
+          loading={<ActivityIndicator />}
+          persistor={Store.persistor}
+        >
           <InAppNotificationProvider>
             <AppNavigator />
           </InAppNotificationProvider>
@@ -37,6 +46,6 @@ export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F5FCFF',
+    backgroundColor: "#F5FCFF",
   },
 });
